5bdb75f237080ec1d5694bf4744855ede2be2541
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-22 16:44:36 +0100

    chore: added footer

---
+++ src/mock/linksFooter.js
@@ -0,0 +1,25 @@
+
+const linksFooter = [
+  {
+    title: 'Section',
+    links: ['Subsection', 'Subsection', 'Subsection', 'Subsection']
+  },
+  {
+    title: 'Section',
+    links: ['Subsection', 'Subsection', 'Subsection', 'Subsection']
+  },
+  {
+    title: 'Section',
+    links: ['Subsection', 'Subsection', 'Subsection', 'Subsection']
+  },
+  {
+    title: 'Section',
+    links: ['Subsection', 'Subsection', 'Subsection', 'Subsection']
+  },
+  {
+    title: 'Section',
+    links: ['Subsection', 'Subsection', 'Subsection', 'Subsection']
+  },
+]
+
+export default linksFooter;
