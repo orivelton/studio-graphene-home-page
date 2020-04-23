8e2bffeb11dc91a89ebbf8b95ffc1772816f9dff
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-24 00:04:28 +0100

    refactor: changed the name

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,10 +1,10 @@
 const getSection = (selector = 'section') => [...document.querySelectorAll(selector)];
 
 const parallax = () => {
-  const position = document.querySelector('.our-menu').getBoundingClientRect().top;
+  const margin = document.querySelector('.our-menu').getBoundingClientRect().top;
   const setStyle = (item, index) => item.setAttribute(
     'style',
-    `margin-top: ${(index === 0 || index === 2) ? position : -position}px`
+    `margin-top: ${(index === 0 || index === 2) ? margin : -margin}px`
   );
 
   getSection('.parallax').forEach((item, index) => setStyle(item, index));
