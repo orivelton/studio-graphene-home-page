2c51e1591143738a55a84f594147edb7c8bc9596
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-23 09:51:51 +0100

    chore: space between links on menu

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -4,8 +4,6 @@ import Stamp from '../../components/Stamp';
 
 const Home = () => (
   <section className="section home">
-    <Stamp textTop={'Gormmet Experience'} text={'Logo'} textBottom={'Gormmet Experience'} />
-
     <div className="big-title uppercase">
       <h1 className="big-title__top">
         The Best Foodie 
