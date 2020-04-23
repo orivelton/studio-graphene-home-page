ff2d670da35d062ae72de298cccb9c58277dcd53
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-23 17:29:07 +0100

    test: unit test

--- src/tests/CurrentSection.test.js
+++ src/tests/CurrentSection.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import CurrentSection from '../components/CurrentSection';
 
-test('renders correctly', () => {
+test('renders correctly CurrentSection', () => {
   const tree = renderer.create(<CurrentSection />).toJSON();
   expect(tree).toMatchSnapshot();
 });
