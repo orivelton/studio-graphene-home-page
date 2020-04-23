87b3d305185979eeab7af2a26b723e2aada84877
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-23 17:33:05 +0100

    Updates

---
+++ precache-manifest.3251ddcb709e140daa15c2e09fc346d3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009e65a30a92b89e2c567daf578bda92",
+    "url": "/studio-graphene-home-page/index.html"
+  },
+  {
+    "revision": "3bd46ee9f83ce3bd09bb",
+    "url": "/studio-graphene-home-page/static/css/main.d9860d50.chunk.css"
+  },
+  {
+    "revision": "f500a68bf8f8340b07a2",
+    "url": "/studio-graphene-home-page/static/js/2.bfa6ec6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studio-graphene-home-page/static/js/2.bfa6ec6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd46ee9f83ce3bd09bb",
+    "url": "/studio-graphene-home-page/static/js/main.c2b3183e.chunk.js"
+  },
+  {
+    "revision": "3ea75057ca38ec232a27",
+    "url": "/studio-graphene-home-page/static/js/runtime-main.78f2d223.js"
+  },
+  {
+    "revision": "0d13c4b3f4c60beca7885a31d8406469",
+    "url": "/studio-graphene-home-page/static/media/DINCondensed-Bold.0d13c4b3.woff"
+  },
+  {
+    "revision": "aa15ad20a94678f93baad801b51403c0",
+    "url": "/studio-graphene-home-page/static/media/DINCondensed-Bold.aa15ad20.woff2"
+  },
+  {
+    "revision": "1a7dff3207a77371fc2ed8826fd7ac18",
+    "url": "/studio-graphene-home-page/static/media/GillSans.1a7dff32.woff2"
+  },
+  {
+    "revision": "727e080fb46fff9490a8099bfab3d628",
+    "url": "/studio-graphene-home-page/static/media/GillSans.727e080f.woff"
+  },
+  {
+    "revision": "de10f57bc0a6653cd36aa7420ad85f09",
+    "url": "/studio-graphene-home-page/static/media/bck-home.de10f57b.png"
+  },
+  {
+    "revision": "383f8405cb7c738856490e139fd08d5b",
+    "url": "/studio-graphene-home-page/static/media/icon.383f8405.woff"
+  },
+  {
+    "revision": "8d44b2f8440e3ad4aaf205b129464193",
+    "url": "/studio-graphene-home-page/static/media/icon.8d44b2f8.eot"
+  },
+  {
+    "revision": "9d9e043a50138ea7292905c961ffe011",
+    "url": "/studio-graphene-home-page/static/media/icon.9d9e043a.svg"
+  },
+  {
+    "revision": "d1ccd8d877cdef8c928739978d70f088",
+    "url": "/studio-graphene-home-page/static/media/icon.d1ccd8d8.ttf"
+  }
+]);
